5cedaf488e736289d25d8a52babf97a509e52163
Messias Junior <messiaslima.03@gmail.com>
2022-10-18 20:59:54 +0100

    feat: user indicator

--- app/src/main/java/io/github/messiaslima/feature/chat/ui/ChatView.kt
+++ app/src/main/java/io/github/messiaslima/feature/chat/ui/ChatView.kt
@@ -1,16 +1,26 @@
 package io.github.messiaslima.feature.chat.ui
 
+import androidx.compose.animation.AnimatedVisibility
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
+import io.github.messiaslima.R
+import io.github.messiaslima.core.database.message.Sender
 import io.github.messiaslima.core.ui.theme.MagentaChatTheme
 import io.github.messiaslima.feature.chat.ui.component.*
 import io.github.messiaslima.feature.chat.usecase.ChatUiModel
@@ -24,12 +34,19 @@ fun ChatView(viewModel: ChatViewModel = viewModel(), onNavigationIconClicked: ()
         val sender by viewModel.sender.collectAsState()
         val messages by viewModel.messages.collectAsState(initial = emptyList())
 
-        TopBar(onNavigationItemClicked = onNavigationIconClicked, onMenuClicked = { viewModel.toggleSender() })
+        TopBar(
+            onNavigationItemClicked = onNavigationIconClicked,
+            onMenuClicked = { viewModel.toggleSender() },
+        )
 
         LazyColumn(modifier = Modifier.weight(1f), reverseLayout = true) {
             items(messages) { item ->
                 ChatItem(item)
             }
+        }
+
+        AnimatedVisibility(visible = sender == Sender.OTHER_USER) {
+            UserIndicator()
         }
 
         BottomBar(modifier = Modifier.fillMaxWidth(), onSendMessageClicked = {
@@ -39,11 +56,38 @@ fun ChatView(viewModel: ChatViewModel = viewModel(), onNavigationIconClicked: ()
 }
 
 @Composable
+private fun UserIndicator() {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .background(color = MaterialTheme.colors.primary),
+        contentAlignment = Alignment.Center
+    ) {
+        Text(
+            text = stringResource(id = R.string.user_indicator),
+            color = Color.White,
+            style = MaterialTheme.typography.body2
+        )
+    }
+}
+
+@Composable
 private fun ChatItem(item: ChatUiModel) {
-    when(item) {
-        is MainUserMessageUiModel -> MainUserMessage(modifier = Modifier.fillMaxWidth(), uiModel = item)
-        is OtherUserMessageUiModel -> OtherUserMessage(uiModel = item)
-        is SectionUiModel -> Section(modifier = Modifier.fillMaxWidth(), sectionUiModel = item)
+    when (item) {
+        is MainUserMessageUiModel -> MainUserMessage(
+            modifier = Modifier.fillMaxWidth(),
+            uiModel = item
+        )
+
+        is OtherUserMessageUiModel -> OtherUserMessage(
+            modifier = Modifier.fillMaxWidth(),
+            uiModel = item,
+        )
+
+        is SectionUiModel -> Section(
+            modifier = Modifier.fillMaxWidth(),
+            sectionUiModel = item,
+        )
     }
 }
 
